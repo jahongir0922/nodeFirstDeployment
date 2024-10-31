0ebc62b767e733c25b79abf7591cd0177f6d1c3b
Jahongir <69594209+jahongir0922@users.noreply.github.com>
2024-10-31 18:43:14 +0500

    netlify deleted

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require("express");
-const serverless = require("serverless-http");
+
 const app = express();
-const router = express.Router();
+
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/prod")(app);
 require("dotenv").config();
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`I have been listened port number of ${port}...`);
 });
-app.use("/.netlify/functions/api", router);
-module.exports.handler = serverless(app);
