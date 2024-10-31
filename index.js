7b054bcca3ad5564b006dc79a3f7bafe26d29851
Jahongir <69594209+jahongir0922@users.noreply.github.com>
2024-10-31 13:25:07 +0500

    5000

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("./startup/config")();
 require("./startup/prod")(app);
 require("dotenv").config();
 
-const port = process.env.PORT || 5002;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`I have been listened port number of ${port}...`);
