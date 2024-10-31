98861eafed605fa6813d01540fdf9b6f39500e6c
Jahongir <69594209+jahongir0922@users.noreply.github.com>
2024-10-31 11:40:16 +0500

    dotenv

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/prod")(app);
-
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
