80f24eba748a3b1b72d0ce3909c66412e3a47625
Jahongir <69594209+jahongir0922@users.noreply.github.com>
2024-10-31 18:02:42 +0500

    netlify

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
+const router = express.Router();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
@@ -11,3 +13,5 @@ const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`I have been listened port number of ${port}...`);
 });
+app.use("/.netlify/functions/api", router);
+module.exports.handler = serverless(app);
