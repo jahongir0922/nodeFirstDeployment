1a668dd756e2f16d46e6dfc30ee2f07d2e84c539
Jahongir <69594209+jahongir0922@users.noreply.github.com>
2024-10-31 13:21:15 +0500

    post 5000

--- config/index.js
+++
@@ -1,7 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-const config = {
-  env: process.env["ENV"] ?? "dev",
-  port: process.env["PORT"] ?? 5000,
-};
-export default config;
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-import config from "./config/index.js";
 const express = require("express");
 const app = express();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/prod")(app);
-// require("dotenv").config();
-const port = config.port;
+require("dotenv").config();
 
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5002;
 
 app.listen(port, () => {
   console.log(`I have been listened port number of ${port}...`);
